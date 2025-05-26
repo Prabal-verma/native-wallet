1efd237fe740be694e3d2effa37bf3ddae541fbb
Prabal Verma <prableverma@gmail.com>
2025-05-27 01:29:27 +0530

    Api complete

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 async function dbinit(){
   try {
-    await sql`CREATE TABLE IF NOT EXISTS transcations(
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
     id SERIAL PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL, 
@@ -26,7 +26,42 @@ async function dbinit(){
   }
 }
 
+app.get("/api/transactions/:userId", async(req,res)=>{
+  try {
+    const {userId} = req.params;
+    console.log(userId);
+    const transcations = await sql`
+      SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+    `
+    res.status(200).json(transcations);
+    
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error in api"});
+    console.log(error);
 
+  }
+})
+
+app.delete("/api/transactions/:id",async(req,res)=>{
+  try {
+    const{id} = req.params;
+    console.log(id);
+
+    if(isNaN(parseInt(id))){
+      return res.status(400).json({message:"Invalid Transaction id"});
+    }
+    const result = await sql`
+    DELETE FROM transactions WHERE id = ${id} RETURNING *
+    `
+    if(result.length ===0){
+      return res.status(404).json({message:"Transaction Not Found"});
+    }
+
+    res.status(200).json({message:"Transaction Delete Successfully"});
+  } catch (error) {
+    console.log("Unabale to Delete")
+  }
+})
 app.post("/api/transactions",async(req,res)=>{
    try {
     const{title,amount,category,user_id}=req.body;
@@ -35,7 +70,7 @@ app.post("/api/transactions",async(req,res)=>{
     }
 
     const transaction = await sql`
-    INSERT INTO transcations(user_id, title, amount, category)
+    INSERT INTO transactions(user_id, title, amount, category)
     VALUES (${user_id}, ${title}, ${amount}, ${category})
     RETURNING *
     `
@@ -45,6 +80,34 @@ app.post("/api/transactions",async(req,res)=>{
    } catch (error) {
     res.status(500).json({message: "Internal Server Error in api"});
    }
+})
+
+app.get("/api/transactions/summary/:userId",async(req,res)=>{
+  try {
+    const {userId} = req.params;
+    console.log("hello",userId);
+
+    const balanceResult = await sql`
+    SELECT COALESCE(SUM(amount),0) as balance FROM transactions WHERE user_id = ${userId}
+    `
+    
+    const incomeResult = await sql`
+    SELECT COALESCE(SUM(amount),0) as income FROM transactions WHERE user_id = ${userId} AND amount > 0
+    `
+    const expenseResult = await sql`
+    SELECT COALESCE(SUM(amount),0) as expense FROM transactions WHERE user_id = ${userId} AND amount < 0
+    `
+    res.status(200).json({
+      balance:balanceResult[0].balance,
+      income:incomeResult[0].income,
+      expense:expenseResult[0].expense
+    })
+    
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error in api"});
+    console.log(error);
+  }
+
 })
 
 dbinit().then(()=>{
