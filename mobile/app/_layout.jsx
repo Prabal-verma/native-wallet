84a93f8cc69a5e42624d212ea70dfd12652395e8
Prabal Verma <prableverma@gmail.com>
2025-05-28 20:46:17 +0530

    Project finnish

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/clerk-expo'
 import { Slot } from 'expo-router'
 import SafeScreen from "./../components/SafeScreen.jsx";
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { StatusBar } from 'expo-status-bar';
 
 
 export default function RootLayout() {
@@ -10,6 +11,7 @@ export default function RootLayout() {
       <SafeScreen>
         <Slot />
       </SafeScreen>
+      <StatusBar style='dark'/>
     </ClerkProvider>
   );
 }
---
+++ mobile/constants/api.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://native-wallet.onrender.com/api"
